5f21f4157e55e05e38f1b7390dec78c8b5d30741
Prithvi Kiran Joshi <prithvikjoshi@gmail.com>
2022-11-27 11:49:03 +0100

    P1 Done

--- P1T123.py
+++ P1T123.py
@@ -1,6 +1,4 @@
-import pandas as pd
-
-from P1T1 import qel, qpeople, B_Area, B_Qh, T_Data, i_Data, B_Name
+from P1T1 import fel, qel, qpeople, B_Area, B_Qh, T_Data, i_Data, B_Name
 import numpy as np
 from pandas import *
 
@@ -15,7 +13,9 @@ def calc_qhour(fkth, fath, ftint, ftext, fksun, fisun, fqp, fqel):
     # fisun = solar irradiance in an hour (W/sq.m.)
     # fqp = heat gains due to people in the given hour
     # fqel = heat gains due to electrical appliances in the given hour
-    q_hour = fath * (((fkth / 1000) * (ftint - ftext)) - (fksun * (fisun / 1000)) - (fqp / 1000)) - (fel * fqel)
+    q_hour = fath * (((fkth / 1000) * (ftint - ftext))
+                     - (fksun * (fisun / 1000))
+                     - (fqp / 1000)) - (fel * fqel)
     return q_hour
 
 
@@ -33,32 +33,32 @@ def calc_ksunavg(fkth, fath, ftint, ftextavg, fisunavg, fqpavg, fqelavg):
     return fksunavg
 
 
-# setting given parameters
-fel = 0.8
+# Setting given internal temperature at 21˚C
 Tint = 21
 
-# lists for k_th, k_sun and Q_tot values
+# Lists for k_th, k_sun and Q_total values initialised
 Kth = list()
 Ksun = list()
 TotQDem = list()
-# list of all hourly building demands (including 0) of all building (list of list)
+# List of all hourly building demands (including 0) of all building (list of list)
 Q_th = []
 
-# iterating over heated areas of each building
+# Iterating over heated areas of each building
 for building in range(len(B_Area)):
-    # starting values
+    # Initialising kth extremities for bisection numerical scheme
     kthleft = 1
     kthright = 12
-    # while no convergence
+    # while sets criteria for convergence of scheme
     while abs(kthleft - kthright) > 10 ** -12:
-        kthmiddle = (kthright + kthleft) / 2
+        kthmiddle = (kthright + kthleft) / 2        # Bisectional value (Next iteration value)
+        # Lists for averaging values of Temperatures and heat gains during +- 1˚C range to calculate ksun
         Textavg = list()
         Qpavg = list()
         Qelavg = list()
         Iavg = list()
-        # iterating over the hourly temperature data
+        # Iterating over the hourly external temperature data
         for hour in range(len(T_Data)):
-            # if temp is between 15 and 17deg: heat gains roughly equal heating load (+- 1deg tolerance)
+            # If temp is between 15˚C and 17˚C: heat gains roughly equal heating load (+- 1˚C tolerance)
             if 15 <= T_Data[hour] <= 17:
                 Textavg.append(T_Data[hour])
                 Qpavg.append(qpeople[hour])
@@ -75,27 +75,26 @@ for building in range(len(B_Area)):
         Qdemleft = list()
         Qdemright = list()
         Qdemmiddle = list()
-        i = 0
-        # iterating over the hourly temperature data
+        # Iterating over the hourly temperature data
         for hour in range(len(T_Data)):
-            i = i + 1
-            # heating activated
+            # If temp is below 16˚C, and it is a working hour: heating activated
             if T_Data[hour] < 16 and qel[0][hour] > 0:
-                qdemleft = calc_qhour(kthleft, B_Area[building], Tint, T_Data[hour], ksunleft, i_Data[hour], qpeople[hour],
-                             qel[building][hour])
+                qdemleft = calc_qhour(kthleft, B_Area[building], Tint, T_Data[hour], ksunleft, i_Data[hour],
+                                      qpeople[hour], qel[building][hour])
                 if qdemleft > 0:
                     Qdemleft.append(qdemleft)
 
-                qdemright = calc_qhour(kthright, B_Area[building], Tint, T_Data[hour], ksunright, i_Data[hour], qpeople[hour],
-                              qel[building][hour])
+                qdemright = calc_qhour(kthright, B_Area[building], Tint, T_Data[hour], ksunright, i_Data[hour],
+                                       qpeople[hour], qel[building][hour])
                 if qdemright > 0:
                     Qdemright.append(qdemright)
 
-                qdemmiddle = calc_qhour(kthmiddle, B_Area[building], Tint, T_Data[hour], ksunmiddle, i_Data[hour], qpeople[hour],
-                               qel[building][hour])
+                qdemmiddle = calc_qhour(kthmiddle, B_Area[building], Tint, T_Data[hour], ksunmiddle, i_Data[hour],
+                                        qpeople[hour], qel[building][hour])
                 if qdemmiddle > 0:
                     Qdemmiddle.append(qdemmiddle)
 
+        # Function values calculated; aimed to make them 0
         f1left = sum(Qdemleft) - B_Qh[building]
         f1right = sum(Qdemright) - B_Qh[building]
         f1middle = sum(Qdemmiddle) - B_Qh[building]
@@ -106,20 +105,20 @@ for building in range(len(B_Area)):
             kthright = kthmiddle
     kth = kthright
     knew = kthleft
+
+    # Once bisection scheme brings closer to the root, Newton's scheme applied for faster convergence
     f1 = 1
-    # while no convergence
+    # while sets criteria for convergence of scheme
     while abs(f1) >= 10 ** -6:
         kth = knew
         Textavg = list()
         Qpavg = list()
         Qelavg = list()
         Iavg = list()
-        # hourly heating demand (including 0) for each building
-        Q_th_building = []
 
-        # iterating over the hourly temperature data
+        # Iterating over the hourly temperature data
         for hour in range(len(T_Data)):
-            # if temp is between 15 and 17deg: heat gains roughly equal heating load (+- 1deg tolerance)
+            # If temp is between 15˚C and 17˚C: heat gains roughly equal heating load (+- 1˚C tolerance)
             if 15 <= T_Data[hour] <= 17:
                 Textavg.append(T_Data[hour])
                 Qpavg.append(qpeople[hour])
@@ -133,42 +132,33 @@ for building in range(len(B_Area)):
         Qdem = list()
         Qdemdash = list()
         for hour in range(len(T_Data)):
-            # heating activated and office hours
+            # If temp is below 16˚C, and it is a working hour: heating activated
             if T_Data[hour] < 16 and qel[0][hour] > 0:
-                qdem = calc_qhour(kth, B_Area[building], Tint, T_Data[hour], ksun, i_Data[hour], qpeople[hour],
-                         qel[building][hour])
-                # only heating needs to ba accounted for
+                qdem = calc_qhour(kth, B_Area[building], Tint, T_Data[hour], ksun, i_Data[hour],
+                                  qpeople[hour], qel[building][hour])
+                # Only heating needs to ba accounted for, not cooling
                 if qdem > 0:
                     Qdem.append(qdem)
-                    Q_th_building.append(qdem)
                     Qdemdash.append(B_Area[building] * (Tint - T_Data[hour]))
-                else:
-                    Q_th_building.append(0)
-            else:
-                Q_th_building.append(0)
+
+        # Function values calculated; aimed to make them 0
         f1 = sum(Qdem) - B_Qh[building]
         f1dash = sum(Qdemdash)
         knew = kth - (f1 / f1dash)
     TotQDem.append(int(sum(Qdem)))
     Kth.append(float('%.4f' % float(knew)))
     Ksun.append(float('%.4f' % float(ksun)))
-    Q_th.append(Q_th_building)
 
-# calculating the thermal properties of the building envelope
+# Calculating the thermal properties of the building envelope
 mcpair = (2.5 / 3600) * 1152
 Uenv = list()
 for value in range(len(Kth)):
     uenv = Kth[value] - mcpair
     Uenv.append(float('%.4f' % float(uenv)))
 
-# outputting the results in a dataframe to check against Tab. 1.1 for verification
+# Outputting the results in a dataframe
 CSVDict = {'Building': B_Name, 'Heating Demands (kWh)': TotQDem, 'kth [W/m2-K]': Kth, 'ksun': Ksun,
            'Uenv (W/m2-K)': Uenv}
 
 CSVdf = DataFrame(CSVDict)
 CSVdf.to_csv('P1T123.csv')
-
-# generating a dataframe with all heating loads of all times of all buildings (including zeros)
-df_Q_th = pd.DataFrame(Q_th)
-# dataframe with the hourly sum of all buildings heating load (including zeros)
-df_Q_th_hourly_sum = df_Q_th.sum()
